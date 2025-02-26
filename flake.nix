9f49fb92ab3fac601402529239e07739231b449f
Anders Kristiansen <palantus@gmail.com>
2025-02-26 13:51:36 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/thinkpad/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
---
+++ switch.sh
@@ -0,0 +1,1 @@
+sudo nixos-rebuild switch --flake /home/ahk/nixos#thinkpad
