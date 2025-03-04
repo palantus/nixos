defe23c5cb2f8ffcadefe01c588824e4e8e6d8f4
Anders Kristiansen <palantus@gmail.com>
2025-03-04 22:05:23 +0100

    nvim improvements

--- home/ahk/common/core/neovim.nix
+++ home/ahk/common/core/neovim.nix
@@ -26,6 +26,11 @@
     telescope.enable = true;
     autocomplete.nvim-cmp.enable = true;
     filetree.neo-tree.enable = true;
+    binds.whichKey.enable = true;
+    git.gitsigns.enable = true;
+    useSystemClipboard = true;
+    terminal.toggleterm.lazygit.enable = true;
+    ui.noice.enable = true;
 
     languages = {
       enableLSP = true;
@@ -35,6 +40,39 @@
       nix.enable = true;
       ts.enable = true;
       rust.enable = true;
+      css.enable = true;
+      html.enable = true;
     };
+
+    keymaps = [
+      {
+        key = "<leader>wq";
+        mode = ["n"];
+        action = ":wq<CR>";
+        silent = true;
+        desc = "Save file and quit";
+      }
+      {
+        key = "<leader>q";
+        mode = ["n"];
+        action = ":q<CR>";
+        silent = true;
+        desc = "Quit";
+      }
+      {
+        key = "\\";
+        mode = ["n"];
+        action = ":Neotree toggle<CR>";
+        silent = true;
+        desc = "Quit";
+      }
+      {
+        key = "<Esc>";
+        mode = ["n"];
+        action = ":nohlsearch<CR>"; # Clear search highlight
+        silent = true;
+        # desc = "Quit";
+      }
+    ];
   };
 }
