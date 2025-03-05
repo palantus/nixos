dc611877f90c843aaf7034e28dca575dd34b9937
Anders Kristiansen <palantus@gmail.com>
2025-03-05 14:32:35 +0100

    Push

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -106,46 +106,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  stylix = {
-    enable = true;
-    image = (lib.custom.relativeToRoot "assets/wallpapers/zen-01.png");
-    #      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-    #      cursor = {
-    #        package = pkgs.foo;
-    #        name = "";
-    #      };
-    #     fonts = {
-    #monospace = {
-    #    package = pkgs.foo;
-    #    name = "";
-    #};
-    #sanSerif = {
-    #    package = pkgs.foo;
-    #    name = "";
-    #};
-    #serif = {
-    #    package = pkgs.foo;
-    #    name = "";
-    #};
-    #    sizes = {
-    #        applications = 12;
-    #        terminal = 12;
-    #        desktop = 12;
-    #        popups = 10;
-    #    };
-    #};
-    opacity = {
-      applications = 1.0;
-      terminal = 1.0;
-      desktop = 1.0;
-      popups = 0.8;
-    };
-    polarity = "dark";
-    # program specific exclusions
-    #targets.foo.enable = false;
-  };
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
