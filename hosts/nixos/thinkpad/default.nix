66edf768a6e1bb85db153461f123c1c1891359b1
Anders Kristiansen <palantus@gmail.com>
2025-03-05 14:35:22 +0100

    Push

--- hosts/nixos/thinkpad/default.nix
+++ hosts/nixos/thinkpad/default.nix
@@ -97,7 +97,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
