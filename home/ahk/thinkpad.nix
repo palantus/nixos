b05ef75ca8e3e14a935eb5660bc10593a0e32489
Anders Kristiansen <palantus@gmail.com>
2025-03-05 13:27:11 +0100

    Laptop user config

---
+++ home/ahk/thinkpad.nix
@@ -0,0 +1,49 @@
+{ ... }:
+{
+  imports = [
+    #
+    # ========== Required Configs ==========
+    #
+    common/core
+
+    #
+    # ========== Host-specific Optional Configs ==========
+    #
+    common/optional/browsers
+    common/optional/desktops # default is hyprland
+    # common/optional/development
+    common/optional/comms
+    # common/optional/helper-scripts
+    # common/optional/gaming
+    # common/optional/media
+    common/optional/tools
+    #
+    # common/optional/atuin.nix
+    common/optional/xdg.nix # file associations
+    common/optional/temp.nix # TODO: move
+    # common/optional/sops.nix
+  ];
+
+
+  #
+  # ========== Host-specific Monitor Spec ==========
+  #
+  # This uses the nix-config/modules/home/montiors.nix module which defaults to enabled.
+  # Your nix-config/home-manger/<user>/common/optional/desktops/foo.nix WM config should parse and apply these values to it's monitor settings
+  # If on hyprland, use `hyprctl monitors` to get monitor info.
+  # https://wiki.hyprland.org/Configuring/Monitors/
+
+  monitors = [
+    {
+      name = "DP-2";
+      width = 1600;
+      height = 900;
+      refreshRate = 60;
+      x = 0;
+      y = 0;
+      # vrr = 1;
+      primary = true;
+      workspace = "1";
+    }
+  ];
+}
