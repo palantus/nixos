b2d9c7ee965aad8cb00ee1644a36628e53e107b2
Anders Kristiansen <palantus@gmail.com>
2025-03-12 12:58:54 +0100

    Add wireguard

--- home/ahk/thinkpad.nix
+++ home/ahk/thinkpad.nix
@@ -18,6 +18,7 @@
     # common/optional/gaming
     # common/optional/media
     common/optional/tools
+    common/optional/vpn
     #
     # common/optional/atuin.nix
     common/optional/xdg.nix # file associations
