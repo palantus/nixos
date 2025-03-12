cbd0ef3bad3cdd47aafb4995176edda01a8584b6
Anders Kristiansen <palantus@gmail.com>
2025-03-12 13:19:06 +0100

    Move wireguard to host

--- hosts/nixos/thinkpad/default.nix
+++ hosts/nixos/thinkpad/default.nix
@@ -32,6 +32,7 @@
         # "hosts/common/optional/scanning.nix" # SANE and simple-scan
         # "hosts/common/optional/thunar.nix" # file manager
         "hosts/common/optional/vlc.nix" # media player
+        "hosts/common/optional/wireguard.nix" # media player
         "hosts/common/optional/wayland.nix" # wayland components and pkgs not available in home-manager
         # "hosts/common/optional/yubikey.nix" # yubikey related packages and configs
         # "hosts/common/optional/zsa-keeb.nix" # Moonlander keeb flashing stuff
