37ccb8e2100e243b8c91b1d577a5203b7b2e4fee
Anders Kristiansen <palantus@gmail.com>
2025-03-26 11:34:32 +0100

    Rofi

--- home/ahk/common/optional/desktops/rofi.nix
+++ home/ahk/common/optional/desktops/rofi.nix
@@ -1,13 +1,38 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }:
+let
+  inherit (config.lib.formats.rasi) mkLiteral;
+in
 {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
     theme = lib.mkForce "fancy";
+    # theme = lib.mkForce{
+    #   "*" = {
+    #     background-color = mkLiteral "#000000";
+    #     foreground-color = mkLiteral "rgba(250, 251, 252, 100%)";
+    #     border-color = mkLiteral "#FFFFFF";
+    #     width = 512;
+    #   };
+    #   "#inputbar" = {
+    #     children = map mkLiteral [ "prompt" "entry" ];
+    #   };
+    #   "#textbox-prompt-colon" = {
+    #     expand = false;
+    #     str = ":";
+    #     margin = mkLiteral "0px 0.3em 0em 0em";
+    #     text-color = mkLiteral "@foreground-color";
+    #   };
+    # };
+    # theme = builtins.toFile "theme.rasi" ''
+    #   @import "../path/to/style-1.rasi"
+    # '';
+    # theme = lib.mkForce (lib.custom.relativeToRoot "assets/rofi-style1.rasi");
     terminal = "ghostty";
 
     extraConfig = {
