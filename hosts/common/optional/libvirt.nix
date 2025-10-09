f6f0f6629ed6c769b533b533c13e540d6e5c34a7
Anders Kristiansen <palantus@gmail.com>
2025-10-09 13:30:47 +0200

    Virt tpm support

--- hosts/common/optional/libvirt.nix
+++ hosts/common/optional/libvirt.nix
@@ -3,12 +3,27 @@
   imports = [ ];
 
   programs.virt-manager.enable = true;
-  virtualisation.libvirtd.enable = true;
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          pkgs.OVMFFull.fd
+        ];
+      };
+    };
+    extraConfig = ''swtpm = "/run/current-system/sw/bin/swtpm"'';
+  };
   virtualisation.spiceUSBRedirection.enable = true;
 
   environment.systemPackages = [
     pkgs.qemu_kvm
     pkgs.qemu
+    pkgs.swtpm
   ];
 
   users.users.${config.hostSpec.username} = {
