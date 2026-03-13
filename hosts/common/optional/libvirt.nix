3491ab91e52de3e6063f480f5f0c81743aa9e176
Anders Kristiansen <palantus@gmail.com>
2026-03-13 09:23:14 +0100

    Fix vms

--- hosts/common/optional/libvirt.nix
+++ hosts/common/optional/libvirt.nix
@@ -21,15 +21,15 @@
   };
   virtualisation.spiceUSBRedirection.enable = true;
 
-  systemd.services.virt-secret-init-encryption = {
-    enable = true;
-    # We override the broken /usr/bin/sh command with a direct path to bash
-    serviceConfig.ExecStart = lib.mkForce (
-      "${pkgs.bash}/bin/bash -c 'umask 0077 && (dd if=/dev/random status=none bs=32 count=1 | ${pkgs.systemd}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
-    );
-    # Ensure the directory exists before the service runs
-    preStart = "mkdir -p /var/lib/libvirt/secrets";
-  };
+  # systemd.services.virt-secret-init-encryption = {
+  #   enable = true;
+  #   # We override the broken /usr/bin/sh command with a direct path to bash
+  #   serviceConfig.ExecStart = lib.mkForce (
+  #     "${pkgs.bash}/bin/bash -c 'umask 0077 && (dd if=/dev/random status=none bs=32 count=1 | ${pkgs.systemd}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
+  #   );
+  #   # Ensure the directory exists before the service runs
+  #   preStart = "mkdir -p /var/lib/libvirt/secrets";
+  # };
 
   systemd.sockets.virtqemud = {
     description = "Libvirt QEMU local socket";
