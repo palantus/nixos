88d351a36b4a13087ac10b5a32770b5767753974
Anders Kristiansen <palantus@gmail.com>
2026-06-22 22:37:55 +0200

    Switch greeter

--- hosts/common/optional/de.nix
+++ hosts/common/optional/de.nix
@@ -1,10 +1,20 @@
-{...}
+{pkgs, ...}
 :{
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
+  # services.displayManager.gdm.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri --remember --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red";
+        user = "greeter";
+      };
+    };
+  };
 
   programs.niri = {
     enable = true;
